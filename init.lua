c9e238626581bc42fd66f7f9fa37ba88e6d08328
Owen Musser <owenmusser29@gmail.com>
2025-04-16 08:29:47 -0400

    Add more layouts and hyper key

--- init.lua
+++ init.lua
@@ -1,45 +1,50 @@
-hs.hotkey.bind({"cmd"}, "Left", function()
+local hyper = {"cmd", "alt", "ctrl", "shift"}
+
+hs.hotkey.bind(hyper, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
-
   f.x = max.x
   f.y = max.y
   f.w = max.w / 2
   f.h = max.h
   win:setFrame(f)
 end)
-hs.hotkey.bind({"cmd"}, "Right", function()
+
+hs.hotkey.bind(hyper, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
-
   f.x = max.x + (max.w / 2)
   f.y = max.y
   f.w = max.w / 2
   f.h = max.h
   win:setFrame(f)
 end)
-hs.hotkey.bind({"cmd"}, "up", function()
-  -- Open Gmail Chat
-  hs.task.new("/usr/bin/open", nil, {
-    "-na", "Google Chrome",
-    "--args",
-    "--new-window",
-    "--app=https://mail.google.com/mail/u/0/#chat/homes"
-  }):start()
 
-  -- Open Google Calendar
-  hs.task.new("/usr/bin/open", nil, {
-    "-na", "Google Chrome",
-    "--args",
-    "--new-window",
-    "--app=https://calendar.google.com"
-  }):start()
+hs.hotkey.bind(hyper, "h", function() hs.execute("open ~/") end)
 
-  hs.alert.show("Opening Chat & Calendar")
+local hostname = hs.host.localizedName()
+hs.alert.show(hostname)
+
+if hostname == "tech-1" then
+  layout = {
+    {"taipo", nil, "EB321HQU (1)", {0, 0, 0.5, 1}, nil, nil},
+    {"Gmail", nil, "EB321HQU (1)", {0.5, 0, 0.5, 0.5}, nil, nil},
+    {"Google Calendar", nil, "EB321HQU (1)", {0.5, 0.5, 0.5, 0.5}, nil, nil},
+    {"Google Chrome", nil, "EB321HQU (2)", {0, 0, 0.5, 1}, nil, nil},
+    {"Ghostty", nil, "EB321HQU (2)", {0.5, 0, 0.5, 1}, nil, nil},
+  }
+elseif hostname == "owen-mini" then
+elseif hostname == "owen-air" then
+end
+
+for _,v in pairs (layout) do
+  local app = v[1]
+  hs.application.launchOrFocus(app)
+end
+hs.timer.doAfter(0.5, function()
+  hs.layout.apply(layout)
 end)
-
-
